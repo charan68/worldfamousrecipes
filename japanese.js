f62c8e5ea47a27643c6c5194cb860a302a6a2203
Sai Charan <124520514+charan68@users.noreply.github.com>
2023-03-27 23:14:14 +0530

    Add files via upload

---
+++ japanese.js
@@ -0,0 +1,20 @@
+
+$.ajax({
+    type: 'get',
+    url: `https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese`,
+    success: function(response){
+        console.log(response);
+        for(let i=0;i<response.meals.length;i++){
+            let newitem = `<div data-aos="fade-down" class="col-md-3 m-2 shadow-lg p-3 mb-5 bg-white rounded text-center">
+            <p>${ response.meals[i].strMeal} </p><a href="https://www.themealdb.com/meal/${response.meals[i].idMeal}">
+            <img src="${response.meals[i].strMealThumb}" class="img-fluid"/> <a/>
+            <p class="mt-3"> <a href="https://www.themealdb.com/meal/${response.meals[i].idMeal}"> <button class="btn btn-primary butcol">Recipe</button></a> </p>
+            </div>`;
+
+            $('#myitems').append(newitem);
+        }
+    },
+    error: function(error){
+        console.log(error);
+    }
+    })
